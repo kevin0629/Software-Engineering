5ad05362b4c7f1aa7ea872c6a1fff069f9b2a274
Kevin Kuo <k113423027@g.ncu.edu.tw>
2024-10-22 01:10:14 +0800

    deploy change

--- deploy.sh
+++ deploy.sh
@@ -15,19 +15,11 @@ sudo mv env .env
 
 sudo apt-get update
 echo "installing python and pip"
-sudo apt-get install -y python3 python3-pip python3-venv
-
-# Create a virtual environment
-echo "creating virtual environment"
-python3 -m venv venv
-
-# Activate the virtual environment
-echo "activating virtual environment"
-source venv/bin/activate
+sudo apt-get install -y python3 python3-pip
 
 # Install application dependencies from requirements.txt
 echo "Install application dependencies from requirements.txt"
-pip install -r requirements.txt
+sudo pip install -r requirements.txt
 
 # Update and install Nginx if not already installed
 if ! command -v nginx > /dev/null; then
@@ -62,8 +54,8 @@ sudo pkill gunicorn
 sudo rm -rf myapp.sock
 
 # # Start Gunicorn with the Flask application
-# # Replace 'app:app' with 'yourfile:app' if your Flask instance is named differently.
+# # Replace 'server:app' with 'yourfile:app' if your Flask instance is named differently.
 # # gunicorn --workers 3 --bind 0.0.0.0:8000 server:app &
 echo "starting gunicorn"
-venv/bin/gunicorn --workers 3 --bind unix:myapp.sock app:app --user www-data --group www-data --daemon
+sudo gunicorn --workers 3 --bind unix:myapp.sock  app:app --user www-data --group www-data --daemon
 echo "started gunicorn 🚀"
