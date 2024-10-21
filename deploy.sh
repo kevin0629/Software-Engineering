3d160cc68ceb6d09ec00a33280abec0aec30108a
Kevin Kuo <k113423027@g.ncu.edu.tw>
2024-10-21 10:40:40 +0800

    minor change

--- deploy.sh
+++ deploy.sh
@@ -54,7 +54,7 @@ sudo pkill gunicorn
 sudo rm -rf myapp.sock
 
 # # Start Gunicorn with the Flask application
-# # Replace 'server:app' with 'yourfile:app' if your Flask instance is named differently.
+# # Replace 'app:app' with 'yourfile:app' if your Flask instance is named differently.
 # # gunicorn --workers 3 --bind 0.0.0.0:8000 server:app &
 echo "starting gunicorn"
 sudo gunicorn --workers 3 --bind unix:myapp.sock  app:app --user www-data --group www-data --daemon
